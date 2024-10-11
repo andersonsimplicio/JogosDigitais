47f472710d1ee944c8f91932ffe0310007533deb
Anderson José Simplício <anderson25eu@gmail.com>
2024-10-10 22:26:17 -0300

    Update quick_sort.py

--- TopicosInovadores/quick_sort.py
+++ TopicosInovadores/quick_sort.py
@@ -1,1 +1,7 @@
 
+def quick_sort(arr,esq:int,dir:int):
+    if esq < dir:
+        p = particionar_lomuto(arr,esq,dir)    
+        quick_sort(arr,esq,p-1)
+        quick_sort(arr,p+1,dir)
+        print(f"Pivot {arr[p-1]}")
